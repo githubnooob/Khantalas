b113aa14b58f30f2bd6f90c3bdfc2dde676092f1
Sanjiv <root@localhost.localdomain>
2018-06-18 08:06:18 +0545

    Date Converter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 import os
 import uploadFile
+from DateConverter import DateConverter
 
 
 app = Flask(__name__)
@@ -13,7 +14,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+FILE_PATH
 app.config['UPLOAD_FOLDER'] = uploadFile.UPLOAD_FOLDER
 app.config['SECRET_KEY']= "siasdjasiodjldsj(S*u293719312.,12391u37912739723901/.,';][;][2209526510"
 
+#initialize database
 db= SQLAlchemy(app)
+
+#dateConvertor
+converter = DateConverter()
+
 
 @app.route("/searchFile",methods=['GET','POST'])
 def search():
@@ -25,33 +31,40 @@ def search():
 def add():
 
 	if request.method == "POST":
-		filename = request.form.get('filename')
-		category  = request.form.get('category')
-		category = change_into_string(category)
-		task_state = request.form.get('task_state')
-		task_state = change_into_string(task_state)
-		message = request.form.get('message')
-		message =change_into_string(message)
-		actual_fileName = request.form.get('actual_filename')
-		actual_fileName = change_into_string(actual_fileName)
+
+		# filename = request.form.get('filename')
+		# category  = request.form.get('category')
+		# category = change_into_string(category)
+		# task_state = request.form.get('task_state')
+		# task_state = change_into_string(task_state)
+		# message = request.form.get('message')
+		# message =change_into_string(message)
+		# actual_fileName = request.form.get('actual_filename')
+		# actual_fileName = change_into_string(actual_fileName)
 		reminderDate = request.form.get('reminder_date')
 		reminderDate=	change_into_string(reminderDate)
-		location = request.form.get('location')
-		location = change_into_string(location)
-		print(filename,category,task_state,message,actual_fileName,reminderDate,location)
-        if 'file' in request.files:	
-        	print ("I am in this section")
-	        file = request.files['file']
-	        if filename =="":
-	        	filename = change_into_string(file.filename)
-	        else:
-	        	filename = change_into_string(filename)	
-	        print file.filename
-	        if not file.filename == '' and uploadFile.allowed_file(file.filename):
-	        	file.save(os.path.join(os.getcwd()+"/static/js/ViewerJS/Files", filename))	
-	        newDataFileAdded = File(filename=filename,category=category,task_state=task_state,message= message,actual_name = actual_fileName,location =location,reminderDate=reminderDate)
-	        db.session.add(newDataFileAdded)
-	        db.session.commit()
+
+		#YearDate for checking 
+		yearDate = reminderDate[0:4]
+		print (check_date(yearDate))
+		
+
+		# location = request.form.get('location')
+		# location = change_into_string(location)
+		# print(filename,category,task_state,message,actual_fileName,reminderDate,location)
+  #       if 'file' in request.files:	
+  #       	print ("I am in this section")
+	 #        file = request.files['file']
+	 #        if filename =="":
+	 #        	filename = change_into_string(file.filename)
+	 #        else:
+	 #        	filename = change_into_string(filename)	
+	 #        print file.filename
+	 #        if not file.filename == '' and uploadFile.allowed_file(file.filename):
+	 #        	file.save(os.path.join(os.getcwd()+"/static/js/ViewerJS/Files", filename))	
+	 #        newDataFileAdded = File(filename=filename,category=category,task_state=task_state,message= message,actual_name = actual_fileName,location =location,reminderDate=reminderDate)
+	 #        db.session.add(newDataFileAdded)
+	 #        db.session.commit()
 		return redirect(url_for('add'))	
 
 	elif request.method =="GET":
@@ -63,6 +76,18 @@ def add():
 
 def change_into_string(value):
 	return value.encode("utf-8")
+
+def check_date(year):
+	year_integer = int(year)
+	current_date = datetime.datetime.now()
+	current_year = current_date.year 
+	return current_year , year_integer
+
+def convert_to_English(year,month,date):
+	converter.setNepaliDate(year, month, date)
+	print(str(converter.getEnglishYear())+"/"+str(converter.getEnglishMonth())+"/"+str(converter.getEnglishDate()))
+
+
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
